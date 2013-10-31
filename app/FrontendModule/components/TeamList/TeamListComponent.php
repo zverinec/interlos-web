c45f360e390206ad2d402ce3ceaa35de05650d3f
Jan Drabek <me@jandrabek.cz>
2013-10-31 21:52:22 +0100

    Fix zero teams

--- app/FrontendModule/components/TeamList/TeamListComponent.php
+++ app/FrontendModule/components/TeamList/TeamListComponent.php
@@ -4,10 +4,14 @@ class TeamListComponent extends BaseListComponent {
 	protected function beforeRender() {
 		$this->getTemplate()->teams = $this->getSource()->orderBy('category', 'ASC')->fetchAssoc("category,id_team");
 		$ids = $this->getSource()->fetchPairs("id_team", "id_team");
-		$this->getTemplate()->competitors = Interlos::competitors()->findAll()
+		if(count($ids) == 0) {
+			$this->getTemplate()->competitors = array();
+		} else {
+			$this->getTemplate()->competitors = Interlos::competitors()->findAll()
 				->where("[id_team] IN %l", $ids)
 				->orderBy("name")
 				->fetchAssoc("id_team,id_competitor");
+		}
 		$this->getTemplate()->categories = array(
 				TeamsModel::COLLEGE	    => "Vysokoškoláci",
 				TeamsModel::HIGH_SCHOOL => "Středoškoláci",
